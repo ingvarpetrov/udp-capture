611dc7b9dc75dc3aec56f2bb11267b0799075e20
Ingvar Petrov <i.petrov@otter-video.com>
2025-06-04 21:46:52 +0000

    entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "[entrypoint.sh] Args: $@"
+echo "[entrypoint.sh] Listing /usr/local/bin:"
+ls -l /usr/local/bin
+
 if [ $# -gt 0 ]; then
+  echo "[entrypoint.sh] Executing: $@"
   exec "$@"
 else
+  echo "[entrypoint.sh] No command provided, running tail."
   exec tail -f /dev/null
 fi 
